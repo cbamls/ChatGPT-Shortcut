ce200fad7d9591809772c4dbf79143cf74ad4d44
liangershu <liangershu@meituan.com>
2023-04-28 14:56:27 +0800

    sc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://nav.6aiq.com/sc",
+  url: "https://nav.6aiq.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/sc",
+  baseUrl: "/sc/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
